5efef7edccd5e1fa704ce4abea7719fe545b1f3a
Nikita <as50503as@gmail.com>
2026-07-02 02:30:37 +0300

    added cpack config

---
+++ ChangeLog.md
@@ -0,0 +1,2 @@
+* Thu Jul 02 2026 Shohiii <as50503as@gmail.com> 0.1.0.0
+- Initial RPM release
---
+++ print.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "print.hpp"
+void print(const std::string& text, std::ostream& out) {
+    out << text;
+}
